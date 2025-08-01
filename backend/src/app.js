619f8d66737b21c51830341309dbd00329767952
Adnan <adnanmkhan2003@gmail.com>
2025-08-01 23:13:05 +0530

    Fixed Wildcard syntax error 3

--- backend/src/app.js
+++ backend/src/app.js
@@ -46,7 +46,7 @@ app.use(errorHandler);
 if(process.env.NODE_ENV == 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-  app.get('/*{splat}', (req, res) => {
+  app.get('/*splat', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
   });
 }
